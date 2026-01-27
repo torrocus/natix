2fbe7b82408f97c3b4e7e6d8cfc97a58da24f162
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2026-01-27 12:10:00 +0100

    Update NATIX metrics script

--- scripts/natix_metrics.rb
+++ scripts/natix_metrics.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
 require 'csv'
 require 'date'
 require 'json'
@@ -5,6 +7,14 @@ require 'net/http'
 require 'uri'
 
 CSV_FILE = 'natix_metrics.csv'
+CSV_HEADERS = [
+  'date',
+  'totalKm',
+  'totalDetections',
+  'totalUsers',
+  'countries',
+  'natixBurned'
+].freeze
 
 OFFICIAL_NATIX_URL = 'https://www.natix.network/'
 NATIX_NETWORK_METRICS_URL = 'https://coverage.natix.network/coverage/v1/metrics/global'
@@ -16,8 +26,8 @@ def append_to_csv(data)
   is_file_empty = !File.exist?(CSV_FILE) || File.size?(CSV_FILE).nil?
 
   CSV.open(CSV_FILE, 'a+', headers: true) do |csv|
-    csv << ['date', 'totalKm', 'totalDetections', 'totalUsers'] if is_file_empty
-    csv << [data['date'], data['totalKm'], data['totalDetections'], data['totalUsers']]
+    csv << CSV_HEADERS if is_file_empty
+    csv << CSV_HEADERS.map { |h| data[h] }
   end
 end
 
@@ -45,7 +55,24 @@ def last_csv_date
   end
 end
 
-today = Date.today.to_s
+def last_natix_burned
+  return nil unless File.exist?(CSV_FILE)
+
+  CSV.open(CSV_FILE, headers: true) do |csv|
+    csv.to_a.last&.[]('natixBurned')
+  end
+end
+
+def validate_csv_schema!
+  return unless File.exist?(CSV_FILE)
+
+  headers = CSV.open(CSV_FILE, headers: true, &:first)&.headers
+  abort 'CSV header mismatch' if headers && headers != CSV_HEADERS
+end
+
+validate_csv_schema!
+
+today = Time.now.utc.to_date.to_s
 exit if last_csv_date == today
 
 fetch_url(OFFICIAL_NATIX_URL)
@@ -53,12 +80,22 @@ json_response = fetch_url(NATIX_NETWORK_METRICS_URL)
 
 raw = JSON.parse(json_response)
 metrics = raw['result']
+abort 'Invalid API response' unless metrics.is_a?(Hash)
+
+natix_burned = BigDecimal(metrics['natixBurned'].to_s)
+               .round(8)
+               .to_s('F')
+
+last_burn = last_natix_burned
+abort 'natixBurned decreased' if last_burn && BigDecimal(natix_burned) < BigDecimal(last_burn)
 
 data = {
   'date' => today,
   'totalKm' => metrics['kmMapped'],
   'totalDetections' => metrics['detections'],
-  'totalUsers' => metrics['drivers']
+  'totalUsers' => metrics['drivers'],
+  'countries' => metrics['countries'],
+  'natixBurned' => natix_burned
 }
 
 append_to_csv(data)
